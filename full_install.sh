73b369431b5daa09a50d9d92a990697f4ff4e84b
Michael McKeirnan <mckeimic@gmail.com>
2013-08-22 17:38:00 -0700

    changed installer structure a lot, again...

---
+++ full_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Installing Vim things..."
+ln -ins ~/.mconfig/config/vim/vimrc ~/.vimrc
+mkdir -p ~/.vim/plugins
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+cd
+
+echo "Linking tmux things..."
+ln -ins ~/.mconfig/config/tmux/tmux.conf ~/.tmux.conf
+
+echo "Installing Zsh things..."
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s $(which zsh)
+ln -ins ~/.mconfig/config/zsh/zshrc ~/.zshrc
+ln -is ~/.mconfig/config/zsh/zshenv ~/.zshenv
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+echo "Linking xmonad things..."
+ln -ins ~/.mconfig/config/xmonad/ ~/.xmonad
+
+echo "Configuring git. Use the information you want to show up in commits"
+read -p "Name: " name
+read -p "Email: " email
+git config --global user.name "$name"
+git config --global user.email "$email"
+
+echo "Linking pythonrc and configuring dir_colors..."
+ln -is ~/.mconfig/config/python/pythonrc ~/.pythonrc
+ln -is ~/.mconfig/assets/dir_colors ~/.dir_colors
+
+echo "Configuring bash things..."
+ln -is ~/.mconfig/config/bash/bashrc
+
+echo "Checking that the colors are still pretty..."
+./mconfig/assets/colorCheck.pl
--- install.sh
+++ install.sh
@@ -1,144 +1,26 @@
 #!/bin/bash
 
-set -e
-set -u
-
-ask_about()
-{
-    while true; do
-
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
-
-        # Ask the Question
-        read -p "$1 [$prompt] " REPLY
-
-        # Default?
-        if [ -z "$REPLY" ]; then
-            REPLY=$default
-        fi
-
-        # Check if reply is valid
-        case "$REPLY" in 
-            Y*|y*) return 0;;
-            N*|n*) return 1;;
-        esac
-    done
-}
-
+echo "Pick your install."
 echo
-read -p "Hi"
 echo
-echo "You should install these things first if you can:"
-echo "Vim, tmux, git, terminator, iTerm2, Xmonad, and Zsh"
-echo "Unless they don't apply to you or you're in a god-forsaken wasteland of an OS of course"
+echo "1.) Full install (requires git)"
+echo "2.) Minimal install (requires nothing)"
 echo
-echo 
-echo "If you can't do the whole git thing either, you should run this instead:"
-echo "curl -sS 'http://mckeimic.com/mconfig/mconfig.zip' > temp.zip && unzip temp.zip"
-echo "I also highly recommend being in your home directory right now."
-if ask_about "Keep going? (Y) Or stop to do that stuff first? (N)" Y; then
-    echo "Great. Enjoy."
-    echo
-    echo
-else
-    exit;
-fi
+echo
+read -p "Option? " option
 
-if ask_about "Clone with git?" Y; then
-    git clone http://github.com/mckeimic/mconfig.git .mconfig
-    install_dir=".mconfig"
-else
-    read -p "Ok then genius, where is all my stuff? " install_dir
-fi
+case "$option" in
+1)    echo "Beginning...."
+      cd
+      git clone https://github.com/mckeimic/mconfig .mconfig
+      ./.mconfig/full_install.sh
+      ;;
+2)    echo "Minimal Install"
+      echo "Curl-ing files down....."
+      curl -L -o mconfig.zip http://github.com/mckeimic/mconfig/zipball/master/
+      unzip -d .mconfig mconfig.zip
+      rm -rf mconfig.zip
+esac
 
-mkdir -p "$install_dir/backup"
-
-if ask_about "Setup Vim?" Y; then
-    touch "~/.vimrc"
-    mv ~/.vimrc "$install_dir/backup/"
-    ln -fs "$install_dir/config/vim/vimrc" ~/.vimrc
-    mkdir -p ~/.vim/bundle
-    vim +BundleInstall +qall
-else 
-    if ask_about "Do you at least want a minimal vim config?" Y;then
-        echo "Someday soon."
-    fi
-fi
-
-if ask_about "Setup Git?" Y; then
-    touch "~/.gitconfig"
-    mv ~/.gitconfig "$install_dir/backup/"
-    ln -fs "$install_dir/config/git/gitconfig" ~/.gitconfig
-    echo "Ok. We're gonna set up Git now."
-    echo "Enter the information you want to show up on your commits: "
-    read -p "Name:  " name
-    read -p "Email: " email
-    git config --global user.name "$name" 
-    git config --global user.email "$email" 
-fi
-
-if ask_about "Setup Tmux?" Y; then
-    touch "~/.tmux.conf"
-    mv ~/.tmux.conf "$install_dir/backup/"
-    ln -fs "$install_dir/config/tmux/tmux.conf" ~/.tmux.conf
-fi
-
-if ask_about "Setup Xmonad?" N; then
-    touch "~/.xmobarrc"
-    mkdir -p ~/.xmonad
-    mv "~/.xmonad" "$install_dir/backup/"
-    mv "~/.xmobarrc" "$install_dir/backup/"
-    ln -fs "$install_dir/config/xmonad/xmonad/" ~/.xmonad/
-    ln -fs "$install_dir/config/xmonad/xmobarrc" ~/.xmobarrc
-fi
-
-if ask_about "Setup Bash?" Y; then
-    touch "~/.bashrc"
-    touch "~/.bashrc.local"
-    mv ~/.bashrc "$install_dir/backup/"
-    cp "$install_dir/assets/dir_colors" ~/.dir_colors
-    ln -fs "$install_dir/config/bash/bashrc" ~/.bashrc
-fi
-
-if ask_about "Setup Zsh?" Y; then
-    set +e
-    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-    if ask_about "Install Zsh Syntax highlighting? (Requires git)" Y; then
-        git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-    fi
-    touch "~/.zshrc"
-    touch "~/.zshrc.local"
-    mv ~/.zshrc "$install_dir/backup/"
-    cp "$install_dir/assets/dir_colors" ~/.dir_colors
-    ln -fs "$install_dir/config/zsh/zshrc" ~/.zshrc
-    set -e
-    if ask_about "Permanently change to Zsh?" Y;then
-        chsh -s $(which zsh)
-    fi
-fi
-
-if ask_about "Setup Terminator?" Y; then
-    mkdir -p ~/.config/terminator
-    mkdir -p "$install_dir/backup/.config/terminator"
-    touch "~/.config/terminator/config"
-    mv ~/.config/terminator/config "$install_dir/backup/.config/terminator/"
-    ln -fs "$install_dir/config/terminator/config" ~/.config/terminator/config
-fi
-
-if ask_about "Setup Gnome-Terminal? (Requires Git)" N;then
-    git clone https://github.com/sigurdga/gnome-terminal-colors-solarized /tmp/colors 
-    /tmp/colors/install.sh
-    rm -rf /tmp/colors
-fi
-
-echo "We made it."
 echo "Enjoy!"
+
