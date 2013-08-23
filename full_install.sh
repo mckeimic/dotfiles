730e01cbfa678b3678e853a84c4d90ee291bfd91
Michael McKeirnan <mckeimic@gmail.com>
2013-08-22 23:03:18 -0700

    fixed xmonad folder link and removed old xmonad backup

--- full_install.sh
+++ full_install.sh
@@ -9,6 +9,7 @@ git clone git://github.com/altercation/vim-colors-solarized.git
 mv vim-colors-solarized ~/.vim/bundle/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cd
+vim +BundleInstall +qall
 
 echo "Linking tmux things..."
 ln -ins ~/.mconfig/config/tmux/tmux.conf ~/.tmux.conf
@@ -19,9 +20,10 @@ chsh -s $(which zsh)
 ln -ins ~/.mconfig/config/zsh/zshrc ~/.zshrc
 ln -is ~/.mconfig/config/zsh/zshenv ~/.zshenv
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+touch ~/.bashrc.local
 
 echo "Linking xmonad things..."
-ln -ins ~/.mconfig/config/xmonad/ ~/.xmonad
+ln -ins ~/.mconfig/config/xmonad/xmonad ~/.xmonad
 
 echo "Configuring git. Use the information you want to show up in commits"
 read -p "Name: " name
@@ -34,6 +36,7 @@ ln -is ~/.mconfig/config/python/pythonrc ~/.pythonrc
 ln -is ~/.mconfig/assets/dir_colors ~/.dir_colors
 
 echo "Configuring bash things..."
-ln -is ~/.mconfig/config/bash/bashrc
+ln -is ~/.mconfig/config/bash/bashrc ~/.bashrc
+touch ~/.bashrc.local
 
 echo 'Further configuration info can be found in .mconfig/assets/further_configuration.md'
